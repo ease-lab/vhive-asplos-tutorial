56d2261a41cba9c7c4dd77142049d93fe659d302
Shyam Jesal <s.jesalpura@gmail.com>
2022-02-24 23:57:19 +0530

    add missing ENV variable in recog function
    Signed-off-by: Shyam Jesal <s.jesalpura@gmail.com>

--- hands-on-aws-4-video-analytics/cleanup.sh
+++ hands-on-aws-4-video-analytics/cleanup.sh
@@ -7,4 +7,4 @@ aws lambda delete-function --function-name decoder-$UNIQUE_ID
 aws iam detach-role-policy --role-name lambda-role-$UNIQUE_ID --policy-arn $LAMBDA_POLICY_ARN
 aws iam  delete-role --role-name lambda-role-$UNIQUE_ID
 aws iam  delete-policy --policy-arn $LAMBDA_POLICY_ARN
-aws s3api delete-bucket --bucket $UNIQUE_ID
+aws s3 rb s3://$UNIQUE_ID --force
